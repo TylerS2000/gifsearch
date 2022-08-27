f90058144ebef6e2a5b1458d6d0e22b68ace8133
TylerS2000 <111829424+TylerS2000@users.noreply.github.com>
2022-08-27 10:36:17 -0400

    Add files via upload

--- Gif Search/gif.js
+++ Gif Search/gif.js
@@ -1,4 +1,6 @@
 async function gif(){
+    let p = document.getElementById("p");
+    while(p.firstChild){p.removeChild(p.lastChild);}
 let search = document.getElementById("search");
 let input= search.value;
 let url = `https://api.giphy.com/v1/gifs/search?api_key=QQqEybWLlZj5qJeRL3evR1z68L78pLP2&q=${input}&limit=25&offset=0&rating=g&lang=en`
@@ -6,10 +8,15 @@ let gifapi= await fetch(url);
 let results = await gifapi.json();
 for(let i = 0; i< results.data.length; i++){let img= document.createElement("img"); 
 img.setAttribute("src",results.data[i].images.fixed_height.url);
-let p = document.getElementById("p");
+
 p.appendChild(img);
+p.innerHTML+="  "
+}
 }
 
+function clears(){
+    let p = document.getElementById("p");
+    while(p.firstChild){p.removeChild(p.lastChild);}
 }
 
 
